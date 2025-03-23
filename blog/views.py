b55d5cffc3cafc9be95dd0e305845761da9a9785
GOYAM JAIN <goyamjain02@gmail.com>
2025-03-23 20:07:33 +0530

    user render for templates

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse  //no longer needed because we are using render
+
+posts = [
+{
+    'author': 'Goyam02',
+    'title': 'Blog Post 1',
+    'content': 'First post content',
+    'date_posted': 'March 23, 2025'
+},
+{
+    'author': 'Jain Goyam',
+    'title': 'Blog Post 2',
+    'content': 'Second post content',
+    'date_posted': 'March 23, 2025'
+}
+
+]
+
+
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
